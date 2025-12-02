c6a825f4deb876a7537dd57c17d94ae9d33cd5d4
Puneet Mehra <mehrapuneet472@gmail.com>
2025-12-02 15:36:45 +0530

    Add files via upload

---
+++ Task_1_Operators/answers.py
@@ -0,0 +1,66 @@
+a = 15
+b = 4
+c = 9
+d = 20
+nums = [4, 9, 15, 25]
+
+# Questions:
+
+# 1. a + b
+# 2. d - c
+# 3. b * 2
+# 4. a % b
+# 5. c > b
+# 6. (a * b) - c
+# 7. d // b
+# 8. (a > c) and (b < d)
+# 9. a in nums
+# 10. c ** 2
+# 11. ((a + c) // b) + (d % a)
+# 12. (a ** b) // c
+# 13. (a < d) or (c == b)
+# 14. nums is nums
+# 15. ((d // b) ** 2) - (a % c)
+
+# Answers:
+
+print(1, a + b)
+print(2, d - c)                      
+print(3, b * 2)                      
+print(4, a % b)                      
+print(5, c > b)                      
+print(6, (a * b) - c)                
+print(7, d // b)                     
+print(8, (a > c) and (b < d))        
+print(9, a in nums)                  
+print(10, c ** 2)                    
+print(11, ((a + c) // b) + (d % a))  
+print(12, (a ** b) // c)
+print(13, (a < d) or (c == b))       
+print(14, nums is nums)              
+print(15, ((d // b) ** 2) - (a % c)) 
+
+
+# Output
+
+# 1. **19**
+# 2. **11**
+# 3. **8**
+# 4. **3**
+# 5. **True**
+# 6. **51**
+# 7. **5**
+# 8. **True**
+# 9. **True**
+# 10. **81**
+# 11. **11**
+# 12. **5625**
+# 13. **True**
+# 14. **True**
+# 15. **4**
+
+
+
+
+
+
